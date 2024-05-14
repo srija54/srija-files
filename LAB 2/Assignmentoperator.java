8eab35ad47b981b1a91d3cfd4c68c20fe5c06a83
srija54 <131280818+srija54@users.noreply.github.com>
2024-05-14 15:32:38 +0530

    Add files via upload

---
+++ LAB 2/Assignmentoperator.java
@@ -0,0 +1,14 @@
+package myfirstprogram;
+
+public class Assignmentoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int n1 = 56 ;
+     String name = "SRIJA"; 
+     System.out.println("number:"+n1);
+     System.out.println("name:"+name);
+     
+	}
+
+}
---
+++ LAB 2/LoginCheckin.java
@@ -0,0 +1,17 @@
+package login;
+
+public class LoginCheckin {
+
+	public static void main(String[] args) {
+		String login ="srija";
+		if(login == "shiva") {
+			System.out.println("user is in valid");
+		}
+		 else{
+		System.out.println("user is logged in");
+		}
+
+	}
+
+}
+  
